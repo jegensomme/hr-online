bcd819930f2f48c3f3682e168e2b155c53620b9e
vitov <vitovt.kublitsky@mail.ru>
2021-03-25 12:28:48 +0300

    Position clean unused imports

--- src/main/java/com/techspirit/casein/model/profile/Position.java
+++ src/main/java/com/techspirit/casein/model/profile/Position.java
@@ -1,13 +1,8 @@
 package com.techspirit.casein.model.profile;
 
 import com.techspirit.casein.model.NamedEntity;
-import com.techspirit.casein.model.course.Course;
-import lombok.*;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "positions")
