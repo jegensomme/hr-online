53f388e2acb6d965ef8ef8a4efc9f0b4def6280d
vitov <vitovt.kublitsky@mail.ru>
2021-03-28 13:57:17 +0300

    Swagger

---
+++ src/main/java/com/techspirit/casein/config/WebMvcConfigAdapter.java
@@ -0,0 +1,19 @@
+package com.techspirit.casein.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class WebMvcConfigAdapter extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
